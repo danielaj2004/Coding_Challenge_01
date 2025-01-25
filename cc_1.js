b1ab5771135213db7beffdd41c031e6ce1beb77e
danielaj2004 <danielajoya12@gmail.com>
2025-01-24 22:35:19 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Variables //
+Let employeeName = "Jane smith";
+const employeeID = 050
+var isActive = true
+
+console.log(employeeName);
+console.log(typepf employeeName);
+console.log(employeeID);
+console.log(typeof employeeID);
+console.log(isActive);
+console.log(typeof isActive);
